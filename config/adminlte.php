7136b7fa5a679824b4cf335b9c413a0c77ddf556
Anish Mathew <anishkmathew@gmail.com>
2021-06-26 21:04:19 +0530

    Project logo and title

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'title' => 'Admin',
-    'title_prefix' => 'Laravel',
-    'title_postfix' => 'Portal',
+    'title' => 'Music Shikshan',
+    'title_prefix' => '',
+    'title_postfix' => '',
 
     /*
     |--------------------------------------------------------------------------
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>Laravel',
+    'logo' => '<b></b>',
     'logo_img' => 'vendor/adminlte/dist/img/LaravelLogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img_class' => 'brand-image',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLaravel',
+    'logo_img_alt' => '',
 
     /*
     |--------------------------------------------------------------------------
