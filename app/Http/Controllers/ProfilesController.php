2e6cbfc7a424b982338c7af360a43f29cfb90841
sherin <sherin.taurus@gmail.com>
2021-08-06 12:18:08 +0530

    feature/MUS-48 student is active to user is active

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -42,7 +42,7 @@ class ProfilesController extends Controller
         ->leftJoin('students', 'students.user_id', '=', 'users.id')
         ->leftJoin('currencies', 'students.currency_id', '=', 'currencies.id')
         ->leftJoin('user_types', 'users.user_type_id', '=', 'user_types.id')
-        ->select(['users.*','students.is_active as is_active','user_types.user_type','currencies.code','currencies.symbol','students.is_registered','countries.name AS country_name',DB::raw('DATE_FORMAT(users.dob, "%d-%m-%Y") as dob'),DB::raw('CONCAT(countries.code," ",users.phone) as phone')])
+        ->select(['users.*','users.is_active as is_active','user_types.user_type','currencies.code','currencies.symbol','students.is_registered','countries.name AS country_name',DB::raw('DATE_FORMAT(users.dob, "%d-%m-%Y") as dob'),DB::raw('CONCAT(countries.code," ",users.phone) as phone')])
         ->findOrFail($id);
         
         return view('profiles.index', compact('user'));
