e66400bf8d73c176153f08c72ff3279d637f995a
Basil M Mathew <basilmmathew17@gmail.com>
2021-08-08 12:57:08 +0530

    students dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\App;
 use App\Models\User;
 use App\Models\TutorClass;
+use App\Models\Classes;
 use Auth;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -41,46 +42,94 @@ class HomeController extends Controller
     {
         $id     = Auth::user()->id;
         $user   = Auth::user();
-        $isStudent  =   $user->hasRole('student');
+
+        /*
+         *
+         * Tutor begins
+         */
+
         $isTutor    =   $user->hasRole('tutor');
-        $isAdmin    =   $user->hasRole('super-admin');
+        if($isTutor){
         
+            $students       =   User::with('student')
+                                ->Join('students', 'students.user_id', '=', 'users.id');
+            $students       =   $students->where('tutors.user_id',$id)
+                                         ->Join('tutors', 'tutors.user_id', '=', 'users.id')
+                                         ->Join('tutor_students', 'tutor_students.tutor_id', '=', 'users.id');
+            $students       =   $students->count();
+
+            $studentInfo    =   DB::table('users')
+                                        ->join('countries', 'users.country_id', '=', 'countries.id')
+                                        ->LeftJoin('students', 'students.user_id', '=', 'users.id')
+                                        //->Join('tutors', 'tutors.user_id', '=', 'users.id')
+                                        ->Join('tutor_students', 'tutor_students.user_id', '=', 'users.id')
+                                        ->where('tutor_students.tutor_id',$id)
+                                        ->leftJoin('courses', 'students.course_id', '=', 'courses.id')
+                                        ->select(['users.*','courses.course','countries.name AS country_name',DB::raw('CONCAT(countries.code," ",users.phone) as phone')])
+                                        ->limit(10)
+                                        ->orderBy('users.created_at','desc')
+                                        ->get();
+            
+            $tutorClass =   TutorClass::leftJoin('users', 'classes.student_user_id', '=', 'users.id');
+            $tutorClass =   $tutorClass->select(['classes.*','users.name',DB::raw('DATE_FORMAT(classes.date, "%d-%b-%Y") as date')]);
+            $tutorClass =   $tutorClass->where('tutor_user_id',$id);
+            $tutorClass =   $tutorClass->limit(10)->orderBy('users.created_at','desc')->get();
+
+            $sms        =   DB::table('sms')
+                                ->select('sms.*','users.name',DB::raw('DATE_FORMAT(sms.sent_on, "%d-%b-%Y %h:%i:%s") as sent_on'))
+                                ->leftjoin('users','users.id','=','sms.from_user_id');
+            $sms        =   $sms->where('to_user_id',$id);
+            $sms        =   $sms->limit(10)->orderby('sent_on','desc')->get();
+
+            return view('dashboard.tutor', compact('students','studentInfo','tutorClass','sms'));
+        }
+        /* Tutor ends
+         *
+         *
+         */ 
+
+
+
+         /*
+          * Student begins   
+          *
+          */
+        $isStudent    =   $user->hasRole('student');
+
+        if($isStudent){
+            $classes        =   DB::table('classes')->where('classes.student_user_id',$id);
+            $classes        =   $classes->count();
+
+            $feesDue        =   DB::table('classes')->where('classes.student_user_id',$id);
+            $feesDue        =   $feesDue->sum('class_fee');
+
+            $credits        =    User::with('student')
+                                ->Join('students', 'students.user_id', '=', 'users.id')
+                                ->where('students.user_id',$id)
+                                ->sum('credits');
+
+            $tutorClass     =   TutorClass::leftJoin('users', 'classes.student_user_id', '=', 'users.id');
+            $tutorClass     =   $tutorClass->select(['classes.*','users.name',DB::raw('DATE_FORMAT(classes.date, "%d-%b-%Y") as date')]);
+            $tutorClass     =   $tutorClass->where('student_user_id',$id);
+            $tutorClass     =   $tutorClass->limit(10)->orderBy('users.created_at','desc')->get();
+
+            $paymentHistory =    User::with('student')
+                                ->Join('payment_histories', 'payment_histories.student_user_id', '=', 'users.id')
+                                ->where('payment_histories.student_user_id',$id)
+                                ->select('payment_histories.*',DB::raw('DATE_FORMAT(payment_histories.payment_date, "%d-%b-%Y %h:%i:%s") as payment_date'))
+                                ->limit(10)->orderby('payment_histories.created_at','desc')->get();
+
+            $sms            =   DB::table('sms')
+                                ->select('sms.*','users.name',DB::raw('DATE_FORMAT(sms.sent_on, "%d-%b-%Y %h:%i:%s") as sent_on'))
+                                ->leftjoin('users','users.id','=','sms.from_user_id');
+            $sms            =   $sms->where('to_user_id',$id);
+            $sms            =   $sms->limit(10)->orderby('sent_on','desc')->get();
+
+            
+            return view('dashboard.student', compact('classes','feesDue','credits','paymentHistory','tutorClass','sms'));
+        }
+
         $users      = User::count();
-        $students   = User::with('student')
-                    ->Join('students', 'students.user_id', '=', 'users.id');
-
-        
-        $tutorClass = TutorClass::leftJoin('users', 'classes.student_user_id', '=', 'users.id');
-        $tutorClass = $tutorClass->select(['classes.*','users.name',DB::raw('DATE_FORMAT(classes.date, "%d-%b-%Y") as date')]);
-        
-
-
-        if($isTutor){
-            $tutorClass = $tutorClass->where('tutor_user_id',$id);
-        }
-
-        if($isTutor){
-            $students   =  $students->where('tutors.user_id',$id)
-                                    ->Join('tutors', 'tutors.user_id', '=', 'users.id')
-                                    ->Join('tutor_students', 'tutor_students.tutor_id', '=', 'users.id');
-                                    
-        }
-
-        $students   =  $students->count();
-        
-        $tutorClass =  $tutorClass->limit(10)->orderBy('users.created_at','desc')->get();
-
-
-        $sms       =  DB::table('sms')
-                            ->select('sms.*','users.name',DB::raw('DATE_FORMAT(sms.sent_on, "%d-%b-%Y %h:%i:%s") as sent_on'))
-                            ->leftjoin('users','users.id','=','sms.from_user_id');
-        
-        if($isTutor){
-            $sms   =  $sms->where('to_user_id',$id);
-        }
-        $sms  =  $sms->limit(10)->orderby('sent_on','desc')->get();
-
-
         $tutors     = User::where('user_type_id', 3)
                     ->Join('tutors', 'tutors.user_id', '=', 'users.id')
                     //->where('users.is_active','1')
@@ -91,28 +140,6 @@ class HomeController extends Controller
                     //->where('users.is_active','1')
                     ->sum('credits');
 
-        $studentInfo = DB::table('users')
-                    ->join('countries', 'users.country_id', '=', 'countries.id')
-                    ->LeftJoin('students', 'students.user_id', '=', 'users.id')
-                    //->Join('tutors', 'tutors.user_id', '=', 'users.id')
-                    ->Join('tutor_students', 'tutor_students.user_id', '=', 'users.id')
-                    ->where('tutor_students.tutor_id',$id)
-                    ->leftJoin('courses', 'students.course_id', '=', 'courses.id')
-                    ->select(['users.*','courses.course','countries.name AS country_name',DB::raw('CONCAT(countries.code," ",users.phone) as phone')])
-                    ->limit(10)
-                    ->orderBy('users.created_at','desc')
-                    ->get();
         
-        $tutorInfo      = [];
-        if($isTutor){
-            $tutorInfo  = DB::table('users')
-                    ->where('users.id',$id)
-                    ->select(['users.*'])
-                    ->where('user_type_id', 3)
-                    ->limit(10)
-                    ->get();
-        }
-
-        return view('home', compact('users','students','tutors','credits','studentInfo','tutorInfo','tutorClass','sms','isStudent','isTutor','isAdmin'));
     }
 }
---
+++ resources/views/dashboard/tutor.blade.php
@@ -0,0 +1,182 @@
+@extends('adminlte::page')
+@section('content_header')
+<h1 class="m-0 text-dark">{{__('dashboard.title')}}</h1>
+@stop
+
+@section('content')
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+         <!-- ./col -->
+           <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>{{$students}}</h3>
+
+                <p>Students</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <!--
+              <a href="{{ URL::to('students/students')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              -->      
+            </div>
+          </div>
+         
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-6 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+             <div class="card-header">
+                <h3 class="card-title">Students
+				      	</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0 table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Name</th>
+                      <th>Gender</th>
+                      <th>Course</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+				  <?php foreach($studentInfo as $key => $student){  ?>
+                    <tr>
+					  <td>{{ $key + 1 }}</td>
+                      <td>{{ $student->name }}</td>
+                      <td>{{ $student->gender }}</td>
+                      <td>{{ $student->course }}</td>
+                    </tr>
+				  <?php } ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <!-- /.card -->
+
+            <!-- DIRECT CHAT -->
+            
+
+            <!-- TO DO List -->
+
+            <!-- /.card -->
+          </section>
+          
+          <!-- /.Left col -->
+          <!-- right col (We are only adding the ID to make the widgets sortable)-->
+          
+          <section class="col-lg-6 connectedSortable">
+
+            <div class="card">
+				<div class="card-header">
+					<h3 class="card-title">
+						Tutors Class
+					</h3>
+				</div>
+			  
+              <!-- /.card-header -->
+              <div class="card-body p-0 table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Name</th>
+                      <th>Date</th>
+          					 </tr>
+                  </thead>
+                  <tbody>
+				  <?php foreach($tutorClass as $key => $tutor){ ?>
+                    <tr>
+					  <td>{{ $key + 1 }}</td>
+                      <td>{{ $tutor->name }}</td>
+                      <td>{{ $tutor->date }}</td>
+                    </tr>
+				  <?php } ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <!-- solid sales graph -->
+            
+            <!-- Calendar -->
+            
+            <!-- /.card -->
+          </section>
+          
+          <!-- right col -->
+        </div>
+		
+		
+		
+		<div class="row">
+          <!-- Left col -->
+            <section class="col-lg-6 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+             <div class="card-header">
+                <h3 class="card-title">SMS
+                </h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0 table-responsive">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Name</th>
+                      <th>Message</th>
+					            <th>Sent On</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+				  <?php foreach($sms as $key => $sm){  ?>
+                    <tr>
+					  <td>{{ $key + 1 }}</td>
+                      <td>{{ $sm->name }}</td>
+                      <td>{{ $sm->message }}</td>
+                      <td>{{ $sm->sent_on }}</td>
+                    </tr>
+				  <?php } ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <!-- /.card -->
+
+            <!-- DIRECT CHAT -->
+            
+
+            <!-- TO DO List -->
+
+            <!-- /.card -->
+          </section>
+         
+          <!-- /.Left col -->
+          
+        </div>
+		
+		
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+
+@stop
+	
