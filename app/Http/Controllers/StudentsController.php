41437721584b0cc053b5d60f1cd89348a1cc2e9c
Anish Mathew <anishkmathew@gmail.com>
2021-09-04 21:39:52 +0530

    Students listing with all the active and inactive students

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -47,7 +47,6 @@ class StudentsController extends Controller
                 ->leftJoin('courses', 'students.course_id', '=', 'courses.id')
                 ->select(['users.*', 'users.is_active as is_active','students.credits','students.display_name', 'students.is_registered', 'courses.course', 'countries.name AS country_name', DB::raw('CONCAT(countries.phone_code," ",users.phone) as phone')])
                 ->where('user_type_id', 4)
-                ->where("users.is_active",1)
                 ->get();
             foreach($data as $d)
             {
